9d93f859b4d9360375f81834641357397cc66c0c
Trias Nur Rahman <contact@triasrahman.com>
2017-04-25 07:14:06 +0700

    Create register controller

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -23,6 +23,38 @@ class Auth extends ApiController
 
     public function register()
     {
+        try {
+
+            /*
+             * Validate input
+             */
+            $data = post();
+
+            // dd($data);
+
+            if (!array_key_exists('password_confirmation', $data)) {
+                $data['password_confirmation'] = post('password');
+            }
+
+            $rules = [
+                'name'     => 'required',
+                'email'    => 'required|email|between:6,255',
+                'password' => 'required|between:4,255',
+            ];
+
+            $validation = Validator::make($data, $rules);
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+
+            // Register, no need activation
+            $user = AuthBase::register($data, true);
+
+            return Response::json(Authorizer::issueAccessToken());
+
+        } catch (Exception $e) {
+            return $this->errorWrongArgs($e->getMessage());
+        }
     }
 
     public function forgot()
